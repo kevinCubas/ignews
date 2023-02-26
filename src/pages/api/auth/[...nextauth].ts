55fe97ad2ba4a65ab1fa5b0f6cecfe849c4eb466
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-02-26 20:54:05 -0300

    feat: faunadb config and save user into database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,32 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { Client as FaunaClient, query as q } from "faunadb";
+
+const fauna = new FaunaClient({
+  secret: process.env.FAUNADB_KEY!,
+});
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
-      authorization: {
-        params: { scope: "read:user"}
-      }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
