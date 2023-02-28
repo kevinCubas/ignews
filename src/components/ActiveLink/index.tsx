0ae0d9ac18439002f49dcf6d44edc3e4a3ea6d0c
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-02-28 00:20:20 -0300

    feat: ActiveLink component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,22 @@
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
+import { ReactNode } from "react"
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactNode;
+  activeClassName: string;
+}
+
+export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const className = asPath ===  rest.href
+    ? activeClassName
+    : '';
+
+  return (
+    <Link {...rest} className={className}>
+      {children}
+    </Link>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import logoImg from "public/images/logo.svg";
+import { ActiveLink } from "../ActiveLink";
 import { SignInButton } from "../SignInButton";
 import style from "./styles.module.scss";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={style['header-container']}>
       <div className={style['header-content']}>
         <Image src={logoImg} alt="" />
         <nav>
-          <Link href={'/'} className={style['active']}>Home</Link>
-          <Link href={"/posts"} prefetch>Posts</Link>
+          <ActiveLink href={'/'} activeClassName={style['active']}>
+            Home
+          </ActiveLink>
+          <ActiveLink href={"/posts"} activeClassName={style['active']} prefetch>
+            Posts
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
